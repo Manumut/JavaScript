f8b8440f1b27fd27180c35bd64360b71f493ac11
unknown <manuel.presentaciongr@gmail.com>
2024-12-11 09:38:48 +0100

    cambios

---
+++ Desarrollo web en entorno cliente/1º Trimestre/apiRest/inc/conexion.php
@@ -0,0 +1,14 @@
+<?php
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "escueladb";
+
+    try{
+        $conexion = new PDO("mysql: host=".$host.";dbname=".$dbname.";charset=UTF8", $username, $password);
+        // Damos la posibilidad de que se vean las tildes
+        // $conexion->set_charset("utf8");
+    }catch(PDOException $e){
+        echo "Ha ocurrido el error: ".$e->getMessage();
+    }
+?>
---
+++ Desarrollo web en entorno cliente/1º Trimestre/apiRest/insertar.php
@@ -0,0 +1,25 @@
+<?php
+    require_once("inc/conexion.php");
+
+    $header = getallheaders();
+    if(isset($_POST["tituloCurso"], $_POST["textoCurso"], $_POST["imgCurso"])){
+        $tituloCurso = $_POST["tituloCurso"];
+        $textoCurso = $_POST["textoCurso"];
+        $imgCurso = $_POST["imgCurso"];
+
+        $consulta = "INSERT INTO cursosdb(tituloCurso, textoCurso, imgCurso) VALUES (?, ?, ?)";
+        $sentencia = $conexion->prepare($consulta);
+        $sentencia->execute([$tituloCurso, $textoCurso, $imgCurso]);
+        if($sentencia){
+            echo json_encode("OK");
+        }
+        
+       
+    }else{
+        header("Location:index.html");
+    }
+
+    
+
+
+?>
